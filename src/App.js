672a5e52f804d6427ca0940d71d4036fd56bc417
AbhayKumar1415 <abhay.kumar@qed42.com>
2018-12-02 16:54:03 +0530

    Added Carousel.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
-
+import OnBoarding from './components/onboarding';
 class App extends Component {
   render() {
     return (
-      <div >
-          <Button variant="contained" color="primary">
-            Thats how you show buttons
-          </Button>
-          <h1>
-            View
-            </h1>
-            <u>https://material-ui.com/demos/buttons/</u>
-           <h1> 
-            for more.
-          </h1>
-          
-      </div>
+      <OnBoarding></OnBoarding>
     );
   }
 }
