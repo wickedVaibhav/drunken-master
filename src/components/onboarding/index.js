bfb33059e6bf36eb33df8b3e72536063e02cb43b
AbhayKumar1415 <abhay.kumar@qed42.com>
2018-12-02 15:51:34 +0530

    + component folder, +onboarding, +preference

---
+++ src/components/onboarding/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class OnBoarding extends Component {
+  render() {
+    return (
+      <div>
+        OnBoarding
+      </div>
+    )
+  }
+}
---
+++ src/components/preference/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Preference extends Component {
+  render() {
+    return (
+      <div>
+        Preference
+      </div>
+    )
+  }
+}
